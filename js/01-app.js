ebd6e8f26b805c0f05df9ddb64a29834f1f1ff9b
Mvazquez18 <martinvazquez095@gmail.com>
2021-08-29 22:40:05 -0300

    Desafío 1 Vazquez

---
+++ js/01-app.js
@@ -0,0 +1,20 @@
+let numero1, numero2, resultado;
+let nombreJuego;
+let precioJuego;
+
+
+alert ('Introduzca el nombre del juego')
+nombreJuego = (prompt ("Ingrese el nombre del juego "));
+
+precioJuego= "El precio final de "
+
+
+
+alert ('¡Gran elección! Ahora introduzca el precio para calcular el valor final con impuestos.')
+numero1 = (prompt ("Ingrese el monto:"));
+
+numero2 = 1.65;
+
+resultado= numero1 * numero2;
+
+alert (precioJuego+nombreJuego+" será de $"+resultado);
